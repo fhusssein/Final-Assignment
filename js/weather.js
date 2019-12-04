d43edd8bd7e49dafaa97d9a76ece9c7a0d5973d3
fhussein0878 <fhussein0878@2A501-G04>
2019-12-04 16:45:19 -0500

    weather api updated

--- js/weather.js
+++ js/weather.js
@@ -1,60 +1,26 @@
-//Geo Location
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(getPositionSucc, getPositionErr);
-} else {
-    alert('geolocation not supported on this device');
-}
 
-//getPositionSucc function
-
-function getPositionSucc(position) {
-    var geoLat = position.coords.latitude.toFixed(5);
-    var geoLong = positon.coords.longitude.toFixed(5);
-    var geoAcc = position.coords.accuracy.toFixed(1);
-
-    
-// Creates an iframe and use the current location data
-var iSource = "https://forecast.io/embed/#lat=" + geoLat + "&lon=" +  geoLong + "&name=Woot&color=#00aaff";
-$('<iframe>') //Creates the element
-    .attr('src', iSource) //Sets the attribute spry:region="myDs"
-    .attr('height', 245) // Set the height
-    .attr('width', "100%") //Set the width
-    .appendTo('#id-weather') //Append to an existing element ID
-
-}
-
-function getPositionErr(error) {
-    switch (error.code) {
-        case error.PERMISSION_DENIED:
-            alert("User denied the request for Geolocation.");
-            break;
-        case error.POSITION_UNAVAILABLE:
-            alert("Location information is unavailable.");
-            break;
-        case error.TIMEOUT:
-            alert("The request to get user location timed out.");
-            break;
-        default:
-            alert("An unknown error occurred.");
-
-    }
-}
-
-/*API Param
-    https://api.darksky.net/forecast/1ae1bc597c328dd37b102aaf47ccff7b/43.465041,-80.523566
-
-
-
-var request = new XMLHttpRequest();
-
-// Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://api.darksky.net/forecast/1ae1bc597c328dd37b102aaf47ccff7b/43.465041,-80.523566?exclude=minutely,hourly,daily,alerts,flags&units=auto', true)
-
-request.onload = function() {
-    //Begin accessing JSON Data here
-}
-
-// Send request
-
-request.send()
-*/
+            function weatherApp (city){
+                var apiID = '137538aff620b92e7ea81528baac2d56';
+                fetch ('https://api.openweathermap.org/data/2.5/weather?q=' + city+ '&appid=' + apiID)
+                .then(function(resp){return resp.json()}) //Converts data fetched to json
+                .then(function(data) {
+                    sendWeatherData(data); //calls sendWeatherData function
+                  })
+                  .catch(function() {
+                    // catch any errors
+                  });
+            }
+            function sendWeatherData (wData){
+                var celcius = Math.round(parseFloat(wData.main.temp)-273.15);
+                
+                //Asigns weather description to description div
+                document.getElementById('description').innerHTML = wData[0].description
+                //Assigns temperature to temp div
+                document.getElementById('temp').innerHTML = celcius + '&deg;';
+                //
+                document.getElementById('location').innerHTML = wData.name;
+            }
+            
+            window.onload = function(){
+                weatherApp('Waterloo,ca');
+            }
