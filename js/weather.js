fbf852b80df277ba60571d8fc4df0e57ade682d4
fhussein0878 <fhussein0878@2A501-G04>
2019-12-04 18:50:06 -0500

    weather api updated and working

--- js/weather.js
+++ js/weather.js
@@ -1,26 +1,35 @@
+const apiID = '137538aff620b92e7ea81528baac2d56';
+if(apiID=='') document.getElementById('temp').innerHTML = ('Remember to add your api key!');
 
-            function weatherApp (city){
-                var apiID = '137538aff620b92e7ea81528baac2d56';
-                fetch ('https://api.openweathermap.org/data/2.5/weather?q=' + city+ '&appid=' + apiID)
-                .then(function(resp){return resp.json()}) //Converts data fetched to json
-                .then(function(data) {
-                    sendWeatherData(data); //calls sendWeatherData function
-                  })
-                  .catch(function() {
-                    // catch any errors
-                  });
-            }
-            function sendWeatherData (wData){
-                var celcius = Math.round(parseFloat(wData.main.temp)-273.15);
-                
-                //Asigns weather description to description div
-                document.getElementById('description').innerHTML = wData[0].description
-                //Assigns temperature to temp div
-                document.getElementById('temp').innerHTML = celcius + '&deg;';
-                //
-                document.getElementById('location').innerHTML = wData.name;
-            }
-            
-            window.onload = function(){
-                weatherApp('Waterloo,ca');
-            }
+function weatherApp( city ) {
+	fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city+ '&appid=' + apiID)  
+	.then(function(resp) { return resp.json() }) // Convert data to json
+	.then(function(data) {
+		sendWeatherData(data); //calls sendWeatherData function
+	})
+	.catch(function() {
+		// catch any errors
+	});
+}
+function sendWeatherData (wData) {
+  var celcius = Math.round(parseFloat(wData.main.temp)-273.15);
+	
+    var description = wData.weather[0].description; 
+	
+	document.getElementById('description').innerHTML = description;
+	document.getElementById('temp').innerHTML = celcius + '&deg;';
+	document.getElementById('location').innerHTML = wData.name;
+  
+  if( description.indexOf('rain') > 0 ) {
+  	document.body.className = 'rainy';
+  } else if( description.indexOf('cloud') > 0 ) {
+  	document.body.className = 'cloudy';
+  } else if( description.indexOf('sunny') > 0 ) {
+  	document.body.className = 'sunny';
+  } else {
+  	document.body.className = 'clear';
+  }
+}
+window.onload = function() {
+	weatherApp( 'Waterloo,ca' );
+}
