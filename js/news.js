d6a0fc4b1d251e0021398975f5f03741d1c99c91
https://github.com/MAMichaud/test.git <37840153+MAMichaud@users.noreply.github.com>
2019-12-12 15:20:54 -0500

    video centered, news api jso fetch added

--- js/news.js
+++ js/news.js
@@ -1,6 +1,25 @@
 const apID = '961f5627e72b4eab86386277445e7f3c';
- if(apiID == '') document.getElementById().innerHTML = ('Remember to add your api key!');
+if (apiID == '') document.getElementById().innerHTML = ('Remember to add your api key!');
+var url = 'https://newsapi.org/v2/top-headlines?' + 'country=ca'
+    + '&apiKey='+ apiID;
+var req = new Request(url);
+function newsApp(){
+    fetch(req).then(function(response){
+        return response.json()})
+        .then(function(data){
+            sendNewsData(data);
+        })
+        .catch(function(){
 
- function newsApp(){
-     fetch('https://newsapi.org/v2/everything?q=bitcoin&apiKey='+apiID+)
- }
+        });
+}
+
+function sendNewsData(nData){
+    //Retrives Data from JSON
+    var author = nData.articles[0].author;
+    var title = nData.articles[0].title;
+    var description = nData.articles[0].description;
+
+
+}
+
